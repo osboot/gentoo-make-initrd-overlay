osboot/gentoo-make-initrd-overlay

commit 57e5d6c3c2f52c11ef83db09f65d975cc333edec

sys-kernel/make-initrd: 2.45.0: turn optional USE-flags into suggestions
Signed-off-by: Gabi Falk <gabifalk@gmx.com>

sys-kernel/make-initrd/make-initrd-2.45.0-r2.ebuild
@@ -1,0 +1,120 @@
+# Copyright 2023  Alexey Gladkov <gladkov.alexey@gmail.com>
+# Copyright 2024  Gabi Falk <gabifalk@gmx.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+EAPI=8
+
+inherit autotools multilib optfeature
+
+DESCRIPTION="Uevent-driven initramfs infrastructure based around udev"
+HOMEPAGE="https://github.com/osboot/make-initrd"
+
+if [[ ${PV} == 9999 ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/osboot/${PN}.git"
+else
+	SRC_URI="https://github.com/osboot/make-initrd/releases/download/${PV}/${P}.tar.xz"
+	KEYWORDS="amd64"
+fi
+
+LICENSE="GPL-3+"
+SLOT="0"
+IUSE="+zlib bzip2 lzma zstd +man"
+
+DEPEND="
+	app-alternatives/cpio
+	app-arch/tar
+	dev-libs/libshell
+	sys-apps/coreutils
+	sys-apps/findutils
+	sys-apps/grep
+	sys-apps/kmod
+	sys-apps/util-linux
+	dev-build/make
+	dev-libs/elfutils
+	virtual/libcrypt:=
+	virtual/udev
+	net-libs/libtirpc
+	zlib? ( sys-libs/zlib )
+	bzip2? ( app-arch/bzip2 )
+	lzma? ( app-arch/xz-utils )
+	zstd? ( app-arch/zstd )
+	man? ( app-text/scdoc )
+"
+RDEPEND="${DEPEND}"
+
+BDEPEND="
+	dev-util/intltool
+	dev-build/autoconf
+	dev-build/automake
+	sys-devel/bison
+	sys-devel/flex
+	virtual/pkgconfig
+"
+
+src_prepare() {
+	default
+
+	eautoreconf
+}
+
+src_configure() {
+	econf \
+		--with-bootdir="${EPREFIX}"/boot \
+		--with-runtimedir="${EPREFIX}/usr/$(get_abi_LIBDIR)"/initramfs \
+		--libexecdir="${EPREFIX}"/usr/libexec \
+		--with-imagename='initramfs-$(KERNEL)$(IMAGE_SUFFIX).img' \
+		--with-busybox \
+		--without-libshell \
+		--with-libelf \
+		--with-zlib=$(usex zlib) \
+		--with-bzip2=$(usex bzip2) \
+		--with-lzma=$(usex lzma) \
+		--with-zstd=$(usex zstd) \
+		$(usex man --with-scdoc --without-scdoc) \
+		#
+}
+
+src_compile() {
+	emake
+	default
+}
+
+src_install() {
+	emake DESTDIR="${D}" install
+
+	libexec="${ED}/usr/libexec/${PN}"
+	projdir="${ED}/usr/share/${PN}"
+
+	rm -vr -- "$projdir/features/guestfs"
+
+	exeinto /usr/lib/kernel/install.d
+	# This module is based on 50-dracut.install script from
+	# sys-kernel/dracut-060_pre20240104-r3 package.
+	newexe "${FILESDIR}"/systemd-make-initrd-v2.install 50-make-initrd.install
+
+	exeinto /etc/kernel/preinst.d
+	newexe "${FILESDIR}"/installkernel-make-initrd-v2.install 50-make-initrd.install
+}
+
+pkg_postinst() {
+	optfeature_header "For underlying volume support:"
+	optfeature "LVM2 support" sys-fs/lvm2[lvm]
+	optfeature "Software RAID support" sys-fs/mdadm
+	optfeature "LUKS support" sys-fs/cryptsetup
+	optfeature "Multipath support" sys-fs/multipath-tools
+	optfeature "iSCSI support" sys-block/open-iscsi
+	optfeature "sshfs support" net-fs/sshfs
+	optfeature "ZFS support" sys-fs/zfs
+
+	optfeature_header "For CPU microcode support:"
+	optfeature "Inter microcode support" \
+		sys-apps/iucode_tool sys-firmware/intel-microcode
+	optfeature "AMD microcode support" sys-kernel/linux-firmware
+
+	optfeature_header "For misc feature support:"
+	optfeature "Extra Boot Config support" dev-util/bootconfig
+	optfeature "Plymouth support" sys-boot/plymouth
+	optfeature "SmartCard support" \
+		dev-libs/opensc sys-apps/pcsc-lite sys-apps/pcsc-tools
+}

commit 7632cc45936fee64265a4bd9d8b43aa0593dd6bf

sys-kernel/make-initrd: 2.45.0-r2: fix optional feature suggestions
The optfeature function handles the second and later arguments as
alternative sets of packages required for feature support.  However, in
our case, all these packages are required for their corresponding
features.  Additionally, fix a typo.

Fixes: 57e5d6c ("sys-kernel/make-initrd: 2.45.0: turn optional USE-flags into suggestions")
Signed-off-by: Gabi Falk <gabifalk@gmx.com>

sys-kernel/make-initrd/make-initrd-2.45.0-r2.ebuild
@@ -108,13 +108,13 @@ pkg_postinst() {
 	optfeature "ZFS support" sys-fs/zfs
 
 	optfeature_header "For CPU microcode support:"
-	optfeature "Inter microcode support" \
-		sys-apps/iucode_tool sys-firmware/intel-microcode
+	optfeature "Intel microcode support" \
+		"sys-apps/iucode_tool sys-firmware/intel-microcode"
 	optfeature "AMD microcode support" sys-kernel/linux-firmware
 
 	optfeature_header "For misc feature support:"
 	optfeature "Extra Boot Config support" dev-util/bootconfig
 	optfeature "Plymouth support" sys-boot/plymouth
 	optfeature "SmartCard support" \
-		dev-libs/opensc sys-apps/pcsc-lite sys-apps/pcsc-tools
+		"dev-libs/opensc sys-apps/pcsc-lite sys-apps/pcsc-tools"
 }